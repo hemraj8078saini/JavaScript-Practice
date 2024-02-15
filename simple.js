c7aa2cf05dfc9715a142c946f5b1210b1aa68598
hemraj <hemrajsaini8078@gmail.com>
2024-02-15 16:36:22 +0530

    "changes"

---
+++ simple.js
@@ -0,0 +1,21 @@
+let a,b;
+function sum(x,y){
+    console.log(x,y)
+    
+}
+const value=()=>{
+    let a=4;
+    let b=6;
+    console.log(a+b)
+    console.log("arrow function")
+}
+let arr=[1,2,3,4,5,6]
+ let arr1 =arr.map((i,index)=>{
+  
+    if(i%2==0)
+    {
+    console.log(i);
+    }
+})
+// sum(4,5);
+// value();
