73c53e8aa2fffbf6fc22e8e733e973415416bef6
hemraj <hemrajsaini8078@gmail.com>
2024-02-20 17:44:46 +0530

    "constructor created"

---
+++ constructor.js
@@ -0,0 +1,11 @@
+function writen(name,age,qualification){
+    this.firstname=name;
+    
+    this.ages=age;
+    this.qualifications=qualification;
+    console.log( this.firstname)
+    console.log( this.ages)
+    console.log( this)
+}
+const details = new  writen("John", 50, "Graduation");
+console.log(details)
