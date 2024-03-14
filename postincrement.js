b3776804fdd94e98a9da9dbc11f06f41b457b549
hemraj <hemrajsaini8078@gmail.com>
2024-03-14 16:58:01 +0530

    Post Increment

---
+++ postincrement.js
@@ -0,0 +1,15 @@
+// var a=5;
+// var b=a++;
+// var c=b++;
+// var d=c--;
+// console.log(a+b+c+d)   21
+
+// var x = 5;
+//                  console.log(x++); 
+//                  console.log(x);    output  5  6
+
+             var a=5;
+             var b=a++;
+             var c=b++;
+             var d=c++;
+             console.log(a+b+c+d);
