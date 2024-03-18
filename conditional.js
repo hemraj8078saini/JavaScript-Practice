eba740a633e0be1c5673727237971279c8309f5a
hemraj <hemrajsaini8078@gmail.com>
2024-03-18 11:54:56 +0530

    "conditions"

---
+++ conditional.js
@@ -0,0 +1,64 @@
+                    //Question 1
+let num1=10;
+let num2=15;
+if(num1>num2){
+    console.log("num is larger");
+}
+else if(num1<num2){
+    console.log("num is smaller");
+}
+else if(num1==num2){
+    console.log("both are equl")
+}
+else{
+    console.log("not valid")
+}
+     
+
+                              //Question 2
+       let light="red";
+         
+       if(light=="green"){
+        console.log("go")
+       }
+       else if(light=="yellow"){
+        console.log("slow")
+       }
+       else{
+        console.log("Stop")
+       }
+                                //Question 3
+        let age=25
+        if(age<18){
+            console.log("Minor")
+        }
+        else if(age>=18 && age<=65){
+            console.log("adult")
+        }
+        else{
+            console.log("Senior")
+        }
+
+                               //Question 4
+                            
+    let precentage=49;
+
+    if(precentage>=90){
+        console.log("A")
+    }
+    else if(precentage<=89 && precentage>=80){
+        console.log("B")
+    }
+    else if(precentage<=79 && precentage>=70){
+        console.log("C")
+    }
+    else if (precentage<=69&& precentage>=60){
+        console.log("D")
+    }
+    else if (precentage<=59 && precentage>=50){
+               
+        console.log("E")
+    }
+    else{
+        console.log("fail")
+    }
