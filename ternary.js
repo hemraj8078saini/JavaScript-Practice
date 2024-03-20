28bbb22f6950a43ffe3a8aab7eacde3e68dae0d9
hemraj <hemrajsaini8078@gmail.com>
2024-03-20 12:44:51 +0530

    ternary

---
+++ ternary.js
@@ -0,0 +1,40 @@
+                                    //   Gretest of three numbers
+// let a=50; let b=80; let c=15;
+
+// (a>b&&a>c)?console.log("a is greater"):(b>a&&b>c)?console.log("b is greater"):console.log("c is greater")
+
+            //    if else condition
+
+// if(a>c&& a>b)
+// {
+//     console.log("a is greater");
+// }
+// else if(b>c && b>a)
+// {
+//     console.log("b is greater")
+// }
+// else {
+//     console.log("c is greater")
+// }
+
+                        //    positive number or negative number
+
+    // let x=5;
+    // x>0?console.log(" number is positive number"):console.log("number is negative number")
+
+                         // Login Program
+        
+     let islogged="true";
+    islogged=="true"?console.log("welcome back"):console.log("please log in")
+
+
+    
+   let  dayofweek="monday";
+    (dayofweek=="satuarday"||dayofweek=="sunday")?console.log("weekend"):console.log("not weekend")
+
+    if(dayofweek=="satuardy" && dayofweek=="sunday" ){
+        console.log("weekend")
+    }
+    else{
+        console.log("not weekend")
+    }
