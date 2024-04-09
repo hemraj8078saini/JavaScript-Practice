210da111f6517b4fa82d2c838b0f3148e389de9f
hemraj <hemrajsaini8078@gmail.com>
2024-04-09 11:57:09 +0530

    "update"

---
+++ string.js
@@ -0,0 +1,19 @@
+let arr=[1,3,2,10,2,4]
+let i=0;
+let sum=0
+
+
+while(i<arr.length){
+    if(arr[i]%2==0){
+           
+    sum=sum+arr[i] 
+    }
+   
+    i++;
+}
+
+console.log(sum);
+
+
+//     calculate sum ,average and count of an array of even numbers.
+
