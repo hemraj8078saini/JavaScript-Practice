bdc26e6e4fe9ec064cbdd35596e635e740a994c8
hemraj <hemrajsaini8078@gmail.com>
2024-04-16 17:40:36 +0530

    changes

---
+++ loops.js
@@ -0,0 +1,11 @@
+let a=[1,2,3,4,5,6,7,8,1,2]
+let str ="rajasthan police";
+var lengths;
+
+for( let i=0;i<str.length;i++)
+{ 
+    let lengths=i+0;
+    console.log(lengths)
+
+}
+// console.log(lengths);
