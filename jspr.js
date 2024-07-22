99b4d65e0f29fbb041817653f09f4ffa7de5fb69
hemraj <hemrajsaini8078@gmail.com>
2024-07-22 15:46:24 +0530

    pr

---
+++ jspr.js
@@ -0,0 +1,14 @@
+console.log("first");
+async function  callstack(){
+// let res=await fetch("https://fakestoreapi.com/products")
+let res= 8;
+console.log(res);
+}
+callstack();
+console.log("last")
+let x=10;
+function hello(){
+    var a=40
+}
+
+console.log(this);
