30c30d3ab30631f5b1088f46884348533489719e
hemraj <hemrajsaini8078@gmail.com>
2024-07-24 17:25:21 +0530

    changes

--- jspr.js
+++ jspr.js
@@ -1,14 +1,18 @@
-console.log("first");
-async function  callstack(){
-// let res=await fetch("https://fakestoreapi.com/products")
-let res= 8;
-console.log(res);
-}
-callstack();
-console.log("last")
-let x=10;
-function hello(){
-    var a=40
-}
+// console.log("first");
+// async function  callstack(){
+// // let res=await fetch("https://fakestoreapi.com/products")
+// let res= 8;
+// console.log(res);
+// }
+// callstack();
+// console.log("last")
+// let x=10;
+// function hello(){
+//     var a=40
+// }
 
-console.log(this);
+const b=3;
+function a(){
+    console.log(b);
+}
+a()
