cbb14d240e335c05a96803f7e7a35b1a2ee8753b
hemraj <hemrajsaini8078@gmail.com>
2024-07-24 17:26:26 +0530

    'changes'

--- jspr.js
+++ jspr.js
@@ -1,15 +1,15 @@
-// console.log("first");
-// async function  callstack(){
-// // let res=await fetch("https://fakestoreapi.com/products")
-// let res= 8;
-// console.log(res);
-// }
-// callstack();
-// console.log("last")
-// let x=10;
-// function hello(){
-//     var a=40
-// }
+console.log("first");
+async function  callstack(){
+// let res=await fetch("https://fakestoreapi.com/products")
+let res= 8;
+console.log(res);
+}
+callstack();
+console.log("last")
+let x=10;
+function hello(){
+    var a=40
+}
 
 const b=3;
 function a(){
